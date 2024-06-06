35ea2305a27809ec213aafe416b0fd34f8185a35
Abia Emmanuel <abiaemma@gmail.com>
2024-06-06 15:15:08 +0100

    refactor: Review job

--- app/jobs/notification_relay_job.rb
+++ app/jobs/notification_relay_job.rb
@@ -5,13 +5,17 @@ class NotificationRelayJob
     notification = Notification.find(id)
 
     template = %(
-            <turbo-stream action="append" target="notifications">
-              <template>
-                <p class="title is-6">Notification #{id}</p>
-              </template>
-            </turbo-stream>
-          )
+      <turbo-stream action="append" target="notifications">
+        <template>
+          <p class="title is-6">Notification #{id}</p>
+        </template>
+      </turbo-stream>
+    )
 
     ActionCable.server.broadcast("notifications:#{notification.recipient_id}", {html: template})
+
+    (1..100).each do |i|
+      ActionCable.server.broadcast("notifications:#{notification.recipient_id}", {html: template})
+    end
   end
 end
